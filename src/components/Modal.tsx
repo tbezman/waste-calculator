ee993309b64f2849d45b4093d557a40bc60e462a
Terence Bezman <terencebezman@gmail.com>
2021-12-27 16:54:14 -0800

    better modal accessibility

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,15 +1,32 @@
 import * as React from 'react'
-import { FC, useCallback } from 'react'
+import { FC, MouseEventHandler, useCallback, useEffect } from 'react'
 
 export const Modal: FC<{ onClose: () => void }> = ({ children, onClose }) => {
-  const handleModalKeyDown = useCallback(
-    (event: React.KeyboardEvent) => {
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === 'Escape' || event.key === 'Enter') {
         onClose()
       }
-    },
-    [onClose]
-  )
+    }
+
+    window.addEventListener('keydown', handleKeyDown)
+
+    return () => window.removeEventListener('keydown', handleKeyDown)
+  }, [onClose])
+
+  useEffect(() => {
+    const handleWindowClick = () => {
+      onClose()
+    }
+
+    window.addEventListener('click', handleWindowClick)
+
+    return () => window.removeEventListener('click', handleWindowClick)
+  }, [onClose])
+
+  const handleModalClick = useCallback<MouseEventHandler>((event) => {
+    event.stopPropagation()
+  }, [])
 
   return (
     <div className="fixed top-0 bottom-0 left-0 right-0 flex flex-col items-center pt-16">
@@ -17,8 +34,8 @@ export const Modal: FC<{ onClose: () => void }> = ({ children, onClose }) => {
 
       <div
         tabIndex={0}
-        onKeyDown={handleModalKeyDown}
-        className="z-10 flex flex-col w-full h-full px-8 py-12 bg-white rounded shadow-xl slide-up md:h-auto max-w-2xl max-h-[95vh] overflow-y-scroll"
+        onClick={handleModalClick}
+        className="z-10 flex flex-col w-full h-full px-8 py-12 bg-white rounded shadow-xl slide-up md:h-auto max-w-2xl max-h-[95vh] overflow-y-auto"
       >
         {children}
       </div>
